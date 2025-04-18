dc4c7ea543be73989987b941e12a15e5c9c3578d
k.woraitthinan <k.woraitthinan@avanade.com>
2025-04-18 14:37:41 +0900

    repository dispatch

---
+++ .github/scripts/repository_dispatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# .github/scripts/repository_dispatch.sh
+# This script sends a repository dispatch event to the GitHub API.
+
+# Load environment variables from .env
+set -a
+source .env
+set +a
+
+curl -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
+  https://api.github.com/repos/kworaitthinan-avanade/github-action-demo/dispatches \
+  -d '{"event_type":"trigger-build"}'
