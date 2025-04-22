c515ecb453aef74e883365d74e2b99d604672aea
k.woraitthinan <k.woraitthinan@avanade.com>
2025-04-22 09:46:35 +0900

    Use Custom Action

---
+++ .github/actions/setup-env/index.js
@@ -0,0 +1,18 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+
+async function run() {
+  try {
+    const nodeVersion = core.getInput('node-version');
+    const registryUrl = core.getInput('registry-url');
+
+    await exec.exec(`echo "Setting up Node.js ${nodeVersion}"`);
+    await exec.exec(`echo "Using registry: ${registryUrl}"`);
+
+    await exec.exec(`npm install`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
